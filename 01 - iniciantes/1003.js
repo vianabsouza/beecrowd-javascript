dfef4c880ba461b6e6c44eec897a1eee32520590
Barbara Viana <vianabsouza@gmail.com>
2024-03-18 15:11:24 -0300

    add 1003 challenge

---
+++ 01 - iniciantes/1003.js
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
+let soma = A + B;
+
+console.log('SOMA = ' + soma);
